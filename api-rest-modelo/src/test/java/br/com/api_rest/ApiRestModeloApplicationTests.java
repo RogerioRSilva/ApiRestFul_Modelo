c6ee78956baf61658c826dc42903ac429c7119f3
RogerioRSilva <rogrodriguessilva2022@gmail.com>
2024-10-03 09:27:56 -0300

    Commit inicial

---
+++ api-rest-modelo/src/test/java/br/com/api_rest/ApiRestModeloApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestModeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
